357024c0798123946b9841478c26d92f0217e720
Keneila <keneilarae@gmail.com>
2025-11-01 10:50:32 -0600

    trivia question

--- src/trivia/trivia.jsx
+++ src/trivia/trivia.jsx
@@ -2,6 +2,22 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export function Trivia() {
+  const [question, setQuestion] = React.useState('Loading question...');
+  const [correctAns, setCorrectAns] = React.useState('');
+  const [ans2, setAns2] = React.useState('');
+  const [ans3, setAns3] = React.useState('');
+  const [ans4, setAns4] = React.useState('');
+
+  React.useEffect(() => {
+    setQuestion('Which is the capital of France?');
+    setCorrectAns('Paris');
+    setAns2('London');
+    setAns3('Berlin');
+    setAns4('Madrid');
+  }, []);
+
+
+
   return (
     <div>
     <div className="container">
@@ -17,31 +33,31 @@ export function Trivia() {
       <div className="container text-center">
         <div>
         <h2>Trivia Game</h2>
-        <p className="text-center"> Which is the capital of France?</p>
+        <p className="text-center"> {question} </p>
         </div>
         <div className="d-flex justify-content-center">
         <table>
           <tr>
             <td>
               <button className="btn btn-success">
-                A. Paris
+                A. {correctAns}
               </button>
             </td>
             <td>
               <button className="btn btn-outline-secondary">
-               B. London
+               B. {ans2}
               </button>
             </td>
           </tr>
           <tr>
             <td>
               <button className="btn btn-outline-secondary">
-               C. Berlin
+               C. {ans3}
               </button>
             </td>
             <td>
               <button className="btn btn-outline-secondary">
-               D. Madrid
+               D. {ans4}
               </button>
             </td>
           </tr>
