94cfb737b430be016e52dbb3f3ad1c39fa3c4bf6
Keneila <keneilarae@gmail.com>
2025-11-06 11:20:48 -0700

    service listening on port

---
+++ service/index.js
@@ -0,0 +1,41 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+const authCookieName = 'token';
+
+let users = [];
+let scores = [];
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+app.get('/trivia', (_req, res) => {
+  res.send({ msg: 'Startup service' });
+});
+
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
