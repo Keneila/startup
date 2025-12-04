153916f92d380dc1d80c81ddfb59d9f77d1a3fbf
Keneila <keneilarae@gmail.com>
2025-12-03 18:09:17 -0700

    index.js

--- service/index.js
+++ service/index.js
@@ -196,6 +196,8 @@ app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
 
-app.listen(port, () => {
+const httpService = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+peerProxy(httpService);
